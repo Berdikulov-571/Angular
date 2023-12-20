0fdb19b5cd84e2e8a93c94d2e009b35ed6857840
berdikulov-571 <mygithub571@gmail.com>
2023-12-20 12:11:07 +0500

    app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'your-project-name';
+  title = 'My First Angular Project';
+  // description = "Berdikulov Sanjarbek";
 }
